622fc8e50b7478905bc4e2e16a97f356b2366aa6
Darrel <128795218+darreleng@users.noreply.github.com>
2025-12-25 14:14:02 +0800

    Added delete item feature

--- inventory-app/controllers/itemsController.js
+++ inventory-app/controllers/itemsController.js
@@ -11,13 +11,38 @@ async function getItemsByCategory(req, res) {
                 : pool.query('SELECT * FROM items WHERE LOWER(category) = LOWER($1)', [categoryName]) 
         ]);
         res.render('index', { categories: categoriesResult.rows, items: itemsResult.rows, currentCategory: categoryName });
-        console.log(itemsResult)
-        console.log(itemsResult.rows)
     } catch (error) {
         console.error(error);
         res.status(500).send("Database error");
     }
 }
 
-module.exports = { getItemsByCategory };
+async function addItemPost(req, res) {
+    const { name, quantity, price, category } = req.body;
+    try {
+        await pool.query(
+            "INSERT INTO items (name, quantity, price, category) VALUES ($1, $2, $3, $4)",
+            [name, quantity, price, category]
+        );
+        res.redirect(`/category/${category || 'all'}`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error adding item");
+    }
+}
+
+async function deleteItemPost(req, res) {
+    const { itemId, category } = req.body;
+    try {
+        await pool.query(
+            "DELETE FROM items WHERE id = $1", [itemId]
+        )
+        res.redirect(`/category/${category || 'all'}`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error deleting item");
+    }
+}
+
+module.exports = { getItemsByCategory, addItemPost, deleteItemPost };
 
--- inventory-app/routes/itemsRouter.js
+++ inventory-app/routes/itemsRouter.js
@@ -3,6 +3,8 @@ const itemsController = require('../controllers/itemsController');
 const itemsRouter = Router();
 
 itemsRouter.get('/', itemsController.getItemsByCategory);
+itemsRouter.post('/addItem', itemsController.addItemPost);
+itemsRouter.post('/deleteItem', itemsController.deleteItemPost);
 itemsRouter.get('/category/:categoryName', itemsController.getItemsByCategory);
 
 module.exports = itemsRouter;
