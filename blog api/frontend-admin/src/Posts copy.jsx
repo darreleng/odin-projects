17e81ea7eda725189b5b5fea7f6b7df262e3d651
Darrel <128795218+darreleng@users.noreply.github.com>
2026-01-07 09:36:20 +0800

    Refactored the getAllPosts query in postModel to include a LEFT JOIN on users

--- blog api/frontend-admin/src/Posts copy.jsx
+++ blog api/frontend-admin/src/Posts copy.jsx
@@ -6,14 +6,16 @@ export async function loader() {
   if (!res.ok) throw new Error(`Response status: ${res.status}`);
   
   const posts = await res.json();
-  const postsWithComments = await Promise.all(posts.map(async (post) => {
+  const postsWithAuthors = await Promise.all(posts.map(async (post) => {
+        const authRes = await fetch(`http://localhost:5000/api/users/${post.author_id}`);
+        const author = await authRes.json();
         const commentsRes = await fetch(`http://localhost:5000/api/posts/${post.id}/comments`);
         const comments = await commentsRes.json();
         const numComments = comments.length;
-    return { ...post, numComments };
+    return { ...post, authorName: author, numComments };
     }));
 
-  return postsWithComments;
+  return postsWithAuthors;
 }
 
 export default function Posts() {
